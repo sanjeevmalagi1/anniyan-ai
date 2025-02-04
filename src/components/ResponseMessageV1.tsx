c6b099f7b5a160ce253cba7aa7c3e690f324a47c
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-02-04 23:32:26 +0530

    Fix loadig text

--- src/components/ResponseMessageV1.tsx
+++ src/components/ResponseMessageV1.tsx
@@ -20,8 +20,8 @@ function ResponseMessageV1(props: ResponseMessageV1Props) {
 
   if (isLoading) {
     return (
-      <div className="w-screen border-t-5 px-40 border-b-5 border-gray-600 w-100">
-        <div className="h-full w-full min-h-50 max-h-60 flex items-center justify-center py-2 text-3xl overflow-y-scroll animate-jump animate-once">
+      <div className="px-5 border-t-5 border-b-5 border-gray-600 shadow-[0px_0px_156px_18px_rgba(65,115,113,0.5)]">
+        <div className="font-serif md:px-40 h-full w-full min-h-40 max-h-50 flex items-center py-2 text-xl md:text-3xl overflow-y-scroll animate-jump animate-once font-extrabold">
           Loading ...
         </div>
       </div>
