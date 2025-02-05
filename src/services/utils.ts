0d3343bf9e2fad6750509c653c5c9affcd9aa2d6
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-02-05 08:49:48 +0530

    Increase the poll count to 10

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -44,7 +44,7 @@ export const getLatestMessage = (messages: IMessage[]): IMessage => {
 
 export async function pollForAssistantResponse(threadId: string, latestMessage: IMessage | undefined) {
   let polling = true;
-  const totalPolls = 7;
+  const totalPolls = 10;
   let currentPoll = 0;
 
   while (polling && currentPoll < totalPolls) {
